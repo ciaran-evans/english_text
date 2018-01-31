8efbfc3c3dba5341c34f8bc952983bdc1590cd14
Ciaran Evans <lughnasace@gmail.com>
2018-01-31 12:35:56 -0500

    adding images and an example cnn on mnist data

--- code/divorce_query_grant_analysis.R
+++ code/divorce_query_grant_analysis.R
@@ -14,18 +14,24 @@ n_div <- length(filelist_div)
 # e.g., with mogrify -gravity Center -extent 96x64 *.png
 filelist_full <- c(filelist_quer, filelist_div)
 idist_full <- get_idist(filelist_full, 64, 96, k)
+#saveRDS(idist_full, file='idist_full_G.RDS')
+saveRDS(idist_full, file='idist_full_y.RDS')
 proto_full <- protoclust(idist_full)
-cut_full <- protocut(proto_full, num_clust)
+cut_full <- protocut(proto_full, 16)
 
 plot_cut(cut_full, filelist_full)
-plot_cluster(cut_div, filelist_div, 1)
+plot_cluster(cut_full, filelist_full, 13)
 
 
 # do the analysis again on one cluster
-subfiles <- filelist_full[cut_full$cl == 5]
+#subfiles <- filelist_full[cut_full$cl %in% c(2:12)]
+subfiles <- filelist_full[cut_full$cl %in% c(4, 5, 6, 7, 8, 11, 12)]
 idist_sub <- get_idist(subfiles, 64, 96, k)
+#saveRDS(idist_full, file='idist_sub_G.RDS')
+saveRDS(idist_full, file='idist_sub_y.RDS')
+
 proto_sub <- protoclust(idist_sub)
 cut_sub <- protocut(proto_sub, num_clust)
 
 plot_cut(cut_sub, subfiles)
-plot_cluster(cut_sub, subfiles, 6)
+plot_cluster(cut_sub, subfiles, 7)
